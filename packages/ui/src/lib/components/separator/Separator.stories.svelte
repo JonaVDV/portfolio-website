12f017cc2f3d906967fc0a2c111a133db88a1176
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-02-20 22:01:00 +0100

    feat: add Separator component with customizable orientation and styles

---
+++ packages/ui/src/lib/components/separator/Separator.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Separator from './Separator.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Separator',
+		component: Separator,
+		argTypes: {
+			orientation: {
+				description: 'The orientation of the separator. Can be either horizontal or vertical.'
+			}
+		},
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default"></Story>
+<Story name="Vertical" args={{ orientation: 'vertical' }} />
---
+++ packages/ui/src/lib/components/separator/Separator.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	interface Props {
+		orientation?: 'horizontal' | 'vertical';
+		'--separator-margin'?: string;
+		'--separator-color'?: string;
+		'--separator-thickness'?: string;
+	}
+
+	let { orientation = 'horizontal' }: Props = $props();
+</script>
+
+<!-- 
+	@component Separator
+	
+	A simple separator component that can be used to separate content. It can be used in both horizontal and vertical orientations. The appearance of the separator can be customized using CSS variables.
+-->
+
+<hr aria-orientation={orientation} data-orientation={orientation} />
+
+<style lang="scss">
+	@use '../../styles/abstracts/' as *;
+
+	hr {
+		margin-block: var(--separator-margin);
+		border: none;
+		border-block-start: var(--separator-thickness, 1px) solid
+			var(--separator-color, $clr-surface-600);
+	}
+
+	hr[data-orientation='vertical'] {
+		writing-mode: vertical-rl;
+		// This is a fix to make sure that the seperator stretches to the full height of its container.
+		//  This will eventually be replaced with height: stretch; once that is supported in all browsers.
+		height: 100%;
+	}
+</style>
