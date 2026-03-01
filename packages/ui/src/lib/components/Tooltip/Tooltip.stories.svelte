445919318a1b69bd6aab822e9a0ecc294aa4bfcd
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-03-01 22:25:08 +0100

    feat: add Tooltip component with customizable positioning and content

---
+++ packages/ui/src/lib/components/Tooltip/Tooltip.stories.svelte
@@ -0,0 +1,107 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Tooltip from './Tooltip.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Tooltip',
+		component: Tooltip
+	});
+</script>
+
+<Story name="Position-try bottom">
+	{#snippet template(args)}
+		<div class="wrapper">
+			<Tooltip {...args} position="bottom">
+				{#snippet trigger({ props })}
+					<button {...props}>Hover me</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is a tooltip!</p>
+				{/snippet}
+			</Tooltip>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Position-try left">
+	{#snippet template(args)}
+		<div class="wrapper">
+			<Tooltip position="left" {...args}>
+				{#snippet trigger({ props })}
+					<button {...props}>Hover me</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is a tooltip!</p>
+				{/snippet}
+			</Tooltip>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Position-try right">
+	{#snippet template(args)}
+		<div class="wrapper">
+			<Tooltip position="right" {...args}>
+				{#snippet trigger({ props })}
+					<button {...props}>Hover me</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is a tooltip!</p>
+				{/snippet}
+			</Tooltip>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Position-try top">
+	{#snippet template(args)}
+		<div class="wrapper">
+			<Tooltip position="top" {...args}>
+				{#snippet trigger({ props })}
+					<button {...props}>Hover me</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is a tooltip!</p>
+				{/snippet}
+			</Tooltip>
+		</div>
+	{/snippet}
+</Story>
+
+<Story name="Multiple tooltips">
+	{#snippet template(args)}
+		<div class="wrapper">
+			<Tooltip position="top" {...args}>
+				{#snippet trigger({ props })}
+					<button {...props}>Tooltip 1</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is the first tooltip!</p>
+				{/snippet}
+			</Tooltip>
+
+			<Tooltip position="bottom" {...args}>
+				{#snippet trigger({ props })}
+					<button {...props}>Tooltip 2</button>
+				{/snippet}
+
+				{#snippet content()}
+					<p>This is the second tooltip!</p>
+				{/snippet}
+			</Tooltip>
+		</div>
+	{/snippet}
+</Story>
+
+<style>
+	.wrapper {
+		height: 100svh;
+		display: grid;
+		place-items: center;
+	}
+</style>
