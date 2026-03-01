d6696542365b954798ffb701390ec20aefc09981
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-03-01 22:26:02 +0100

    feat: add File component with input styling and default story

---
+++ packages/ui/src/lib/components/Form/File.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import File from './File.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Form/File',
+		component: File,
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default"></Story>
---
+++ packages/ui/src/lib/components/Form/File.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let { ...rest } = $props();
+</script>
+
+<input type="file" {...rest} />
+
+<style lang="scss">
+	@use '../../styles/abstracts/' as *;
+
+	input[type='file'] {
+		border: 3px dashed;
+
+		border-color: $clr-brand-400;
+		stroke-dashoffset: 5px;
+		text-align: center;
+
+		> :global(label) {
+			display: none;
+			color: transparent;
+		}
+
+		&:hover {
+			border-color: $clr-brand-500;
+		}
+	}
+</style>
