f96d6ee67877cf6c9258c63142b9213b50fd6f6a
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-04-19 21:57:46 +0200

    feat(ui): addplugin-icons + update deps and types

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,12 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import browerslist from 'browserslist';
+import icons from 'unplugin-icons/vite';
 import { browserslistToTargets, Features, type PseudoClasses } from 'lightningcss';
 
 let targets = browserslistToTargets(browerslist('> 0.25%, not dead'));
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		icons({
+			compiler: 'svelte'
+		})
+	],
 
 	css: {
 		// transformer: 'lightningcss',
