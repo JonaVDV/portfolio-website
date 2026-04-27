4c4d71d299806630b7c0e1831f931a6e38a5bd68
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-04-27 21:40:38 +0200

    feat: add section and provider to exports

--- packages/ui/src/lib/components/Sidebar/exports.ts
+++ packages/ui/src/lib/components/Sidebar/exports.ts
@@ -1,5 +1,7 @@
 export { default as Root } from './Sidebar.svelte';
+export { default as Provider } from './SidebarProvider.svelte';
 export { default as Footer } from './SidebarFooter.svelte';
 export { default as Header } from './SidebarHeader.svelte';
 export { default as Group } from './SidebarGroup.svelte';
 export { default as Item } from './SidebarItem.svelte';
+export { default as Section } from './SidebarSection.svelte';
