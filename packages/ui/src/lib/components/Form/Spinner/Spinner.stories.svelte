b945c20a2f1d673dee94e967e3ebe9c8f382fc81
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-06-08 22:24:38 +0200

    feat: add spinner component

---
+++ packages/ui/src/lib/components/Form/Spinner/Spinner.stories.svelte
@@ -0,0 +1,12 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Spinner from './Spinner.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Spinner',
+		component: Spinner,
+		tags: ['autodocs'],
+	});
+</script>
+
+<Story name="Default" args={{ size: '20px' }}></Story>
