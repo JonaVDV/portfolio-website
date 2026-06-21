a6fe260f46d1dd671bf7daab1b3b20b826d22237
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-06-21 18:00:55 +0200

    feat: make kbd a bit better

--- packages/ui/src/lib/components/Kbd/Kbd.svelte
+++ packages/ui/src/lib/components/Kbd/Kbd.svelte
@@ -1,24 +1,11 @@
 <script module>
-	/**
-	 * The command icon
-	 */
 	export const cmd = '⌘';
-	/**
-	 * The control icon
-	 */
 	export const ctrl = '⌃';
-	/**
-	 * The option/alt icon
-	 */
 	export const alt = '⌥';
-	/**
-	 * The shift icon
-	 */
 	export const shift = '⇧';
 </script>
 
 <script lang="ts">
-	import type { Snippet } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	interface Props extends HTMLAttributes<HTMLElement> {}
@@ -32,22 +19,26 @@
 
 <style lang="scss">
 	kbd {
-		--kbd-padding: 0.25rem 0.5rem;
-		--kbd-background: oklch(0.269 0 0);
-		--kbd-color: oklch(1 0 0 / 0.4);
-		--kbd-border-radius: 0.375rem;
-		--kbd-border: 0;
+		--_kbd-padding: var(--kbd-padding, 0.25rem 0.5rem);
+		--_kbd-background: var(--kbd-background, oklch(0.269 0 0));
+		--_kbd-color: var(--kbd-color, oklch(1 0 0 / 0.4));
+		--_kbd-border-radius: var(--kbd-border-radius, 0.375rem);
+		--_kbd-border: var(--kbd-border, 0);
+		--_kbd-font-size: var(--kbd-font-size, 0.75em);
+		--_kbd-font-weight: var(--kbd-font-weight, 500);
 		display: inline-flex;
 		align-items: center;
 		user-select: none;
 		justify-content: center;
-		padding: var(--kbd-padding);
-		border-radius: var(--kbd-border-radius);
+		padding: var(--_kbd-padding);
+		border-radius: var(--_kbd-border-radius);
 		flex-wrap: nowrap;
 		line-height: 1;
 		white-space: nowrap;
-		color: var(--kbd-color);
-		background-color: var(--kbd-background);
-		border: var(--kbd-border);
+		color: var(--_kbd-color);
+		background-color: var(--_kbd-background);
+		border: var(--_kbd-border);
+		font-size: var(--_kbd-font-size);
+		font-weight: var(--_kbd-font-weight);
 	}
 </style>
