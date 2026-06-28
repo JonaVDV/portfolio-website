a9a4d31a9b9083e3fee232812f9a60bfdf70c261
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2026-06-28 20:38:45 +0200

    feat: imporve badge with variants.css

--- packages/ui/src/lib/components/Badge/Badge.svelte
+++ packages/ui/src/lib/components/Badge/Badge.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements';
+	import './Badge.variants.css';
 
 	interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
 		variant?: BadgeVariant;
@@ -24,6 +25,9 @@
 		padding: var(--badge-padding, 0.25em 0.75em);
 		font-size: var(--badge-font-size, var(--fs-300));
 		font-weight: var(--badge-font-weight, var(--fw-medium));
+		letter-spacing: var(--badge-letter-spacing, 0.05em);
+		text-transform: var(--badge-text-transform, uppercase);
+		border: var(--badge-border-width, 0) var(--badge-border-style, solid) var(--_badge-border-color, currentColor);
 		color: var(--badge-color, var(--clr-surface-900));
 		background-color: var(--badge-background, var(--clr-surface-300));
 		border-radius: var(--badge-border-radius, 100vw);
@@ -32,23 +36,10 @@
 		white-space: nowrap;
 	}
 
-	.badge[data-variant='success'] {
-		--badge-color: var(--badge-success-color, var(--clr-teal-400));
-		--badge-background: var(--badge-success-background, oklch(from var(--badge-color) l c h / 15%));
-	}
-
-	.badge[data-variant='warning'] {
-		--badge-color: var(--badge-warning-color, var(--clr-amber-500));
-		--badge-background: var(--badge-warning-background, oklch(from var(--badge-color) l c h / 15%));
-	}
-
-	.badge[data-variant='error'] {
-		--badge-color: var(--badge-error-color, var(--clr-red-400));
-		--badge-background: var(--badge-error-background, oklch(from var(--badge-color) l c h / 15%));
-	}
-
-	.badge[data-variant='info'] {
-		--badge-color: var(--badge-info-color, var(--clr-blue-400));
-		--badge-background: var(--badge-info-background, oklch(from var(--badge-color) l c h / 15%));
+	.badge[data-variant] {
+		--_badge-background-alpha: var(--badge-background-alpha, 15%);
+		--_badge-border-alpha: var(--badge-border-alpha, 50%);
+		--_badge-border-color: var(--badge-border-color, oklch(from var(--badge-color) l c h / var(--_badge-border-alpha)));
+		background-color: var(--badge-background, oklch(from var(--badge-color) l c h / var(--_badge-background-alpha)));
 	}
 </style>
